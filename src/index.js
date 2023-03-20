08c94ae82e9b57e232b5b473df79b31ffd782a2f
Bartek <Barteknr1@gmail.com>
2023-03-20 19:36:55 +0100

    A

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector("#search-box");
+
+// const fetchCountries()
+// https://restcountries.com/#api-endpoints-v3-name
+
 Notiflix.Notify.success("Hi");
+
+fetch("https://restcountries.com/#api-endpoints-v3-name")
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        document.body.innerHTML = `${data.id}`
+    })
+    .catch(error => console.log(error))
